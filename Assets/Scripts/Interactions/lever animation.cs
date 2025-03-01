b981d2d281246a44dd23c9e4f692b5daf503188f
Deathnoisee <adlenalger2005@gmail.com>
2025-03-01 18:10:24 +0100

    Fully working keys with shortcuts

--- Assets/Scripts/Interactions/lever animation.cs
+++ Assets/Scripts/Interactions/lever animation.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class leveranimation : MonoBehaviour
 {
     public Animator animator;
-
+    
     public void interact ()
     {
         animator.SetBool("clicked", true);
